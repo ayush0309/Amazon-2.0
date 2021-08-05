413bcf299184c4d2a9b5ac3054c0bca04bf6e318
Ayush Garg <73277742+ayush0309@users.noreply.github.com>
2021-08-05 21:06:23 +0530

    Connected the Cart Page and Firebase

---
+++ firebase.js
@@ -0,0 +1,12 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyCpNvmTp2KE_2mgpG863UpvQQE1VAWIHgw",
+    authDomain: "two-clone-5a61a.firebaseapp.com",
+    projectId: "two-clone-5a61a",
+    storageBucket: "two-clone-5a61a.appspot.com",
+    messagingSenderId: "54821603080",
+    appId: "1:54821603080:web:312875318e76d896260ebd",
+    measurementId: "G-JYTE0ZJ1M2"
+};
+  // Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
