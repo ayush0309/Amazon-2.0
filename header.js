3b9454046d5d6e1fa0458ef6623ce5c351bc0afd
Ayush Garg <73277742+ayush0309@users.noreply.github.com>
2021-08-06 14:13:27 +0530

    Counter for items in the cart

---
+++ header.js
@@ -0,0 +1,25 @@
+
+function getCartItems() {
+    db.collection("cart-items").onSnapshot((snapshot) => {
+        let totalCount = 0;
+        snapshot.docs.forEach((doc)=>{
+            totalCount += doc.data().quantity;
+        })
+        setCartCounter(totalCount);
+    })
+}
+
+function setCartCounter(totalCount) {
+    // cart-item-number
+    if(totalCount == 0) {
+        document.querySelector(".cart-item-number").classList.add("opacity-0");
+    }
+    else {
+        document.querySelector(".cart-item-number").classList.remove("opacity-0");
+        
+        document.querySelector(".cart-item-number").innerText = totalCount;
+    }
+    
+}
+
+getCartItems();
